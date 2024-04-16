4ac51dfd0c59257b2a931ef853ef4cf25695f6d8
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2024-04-16 15:14:49 -0300

    exercicio 11 finalizado

--- exercicios/exercicio11.js
+++ exercicios/exercicio11.js
@@ -1,2 +1,14 @@
 const prompt = require('prompt-sync')();
 
+let numeros;
+
+do {
+    numeros = parseInt(prompt("Digite o número: "));
+    if (numeros === null || numeros < 0) break;
+
+    if (numeros % 2 === 0) {
+        console.log("O número é Par!");
+    } else {
+        console.log("O número é Ímpar!");
+    }
+} while (numeros >= 0);
