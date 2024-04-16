675b327960d4eae493dc6ed573fc47ace5456386
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2024-04-16 16:20:57 -0300

    Exercicio 14 finalizado

---
+++ exercicios/exercicio14.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync')();
+
+let numeros;
+let soma = 0;
+let quantidade = 0;
+
+while (numeros !== 0) {
+    numeros = parseFloat(prompt("Digite o número: "));
+    if (numeros !== 0) {
+        soma += numeros;
+        quantidade++;
+    }
+}
+
+if (quantidade > 0) {
+    let media = soma / quantidade;
+    console.log(`Média aritmética dos números digitados: ${media}`);
+}
